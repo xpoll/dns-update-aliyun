b4764e248fa692a045aa7953526fce762cbbfdf9
xpoll <blmdz521@126.com>
2018-12-18 17:13:17 +0800

    '提交'

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # dns-update-aliyun
-dns-update-aliyun
+始: 家里光猫是提供外网的，但需要光猫映射，光猫映射已添加OK，无奈外网IP经常变动，时常需要我去百度下我的IP才能在外网操作，我本来就是有域名的，所以这个步骤就变成了:
+>百度IP->登陆阿里云域名解析->修改解析
+
+烦躁，就找了下域名解析的api，然后就有了这个jar
+
+### 首先，利用外部能访问的地址解决查看自己外网IP的问题，内部无法获取外网IP的〒▽〒
+
+1. 找个可以用的网址利用代码获取解析一下
+2. 自己有服务器返回一下
+ 
+
+		// 如nginx直接匹配返回json格式的IP
+	    location = /ip {
+	    	default_type application/json;
+	    	return 200 '{"ip":"$remote_addr"}';
+	    }
+	[https://blmdz.cn/ip](https://blmdz.cn/ip "https://blmdz.cn/ip") 返回格式为: {"ip":"127.0.0.1"}
+    
+### 获取阿里云accessKey
+
+1. 控制台 -> 头像下accessKey -> 子用户【或[链接](https://ram.console.aliyun.com/users "链接")】
+2. 新建 编程访问打勾, 建好就有了, 需要授权
+3. 授权, 搜索dns都打兑上, 确定over
+
+### install
+其他就不多说了，代码可以自己看，逻辑非常简单
+
+    git clone https://github.com/xpoll/dns-update-aliyun.git
+	cd dns-update-aliyun
+    mvn clean install
+    // 查询domain列表
+    java -jar target/dns-update-aliyun-0.0.1-SNAPSHOT.jar query accessKeyId accessKeySecret
+    // 修改映射
+    java -jar target/dns-update-aliyun-0.0.1-SNAPSHOT.jar update accessKeyId accessKeySecret IP_URL IP_CODE recordId
+
+在这里 query 和 update 是固定参数 accessKeyId、accessKeySecret就不用说了必须参数，
+
+IP_URL 修改必须参数，能获取外网的地址需要返回json格式，例如上文: https://blmdz.cn/ip
+
+IP_CODE 修改必须参数，返回json格式ip的key，例如上文: ip
+
+recordId 修改必须参数，修改域名映射的id，通过query可以查到
+
+样例结果如下
+    
+    $ java -jar target/dns-update-aliyun-0.0.1-SNAPSHOT.jar query accessKeyId accessKeySecret
+	...
+    A 123456 abc.aabbcc.com 121.121.121.121
+	...
+    $ java -jar target/dns-update-aliyun-0.0.1-SNAPSHOT.jar update accessKeyId accessKeySecret https://blmdz.cn/ip ip 123456
+    时间: 2018-12-12 12:12:12, 原IP: 121.121.121.121, 现IP: 122.122.122.122, 需要更新
+    更新成功
+    $ java -jar target/dns-update-aliyun-0.0.1-SNAPSHOT.jar update accessKeyId accessKeySecret https://blmdz.cn/ip ip 123456
+    时间: 2018-12-12 12:12:12, 原IP: 122.122.122.122, 现IP: 122.122.122.122, 不需要更新
+
+
+可以看代码修改成自己想要的样子，
+while(true) 也好、运行一次也好
---
+++ src/main/java/cn/blmdz/dns/DemoListDomains.java
@@ -0,0 +1,220 @@
+package cn.blmdz.dns;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import com.alibaba.fastjson.JSONObject;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.alidns.model.v20150109.DescribeDomainRecordInfoRequest;
+import com.aliyuncs.alidns.model.v20150109.DescribeDomainRecordInfoResponse;
+import com.aliyuncs.alidns.model.v20150109.DescribeDomainRecordsRequest;
+import com.aliyuncs.alidns.model.v20150109.DescribeDomainRecordsResponse.Record;
+import com.aliyuncs.alidns.model.v20150109.DescribeDomainsRequest;
+import com.aliyuncs.alidns.model.v20150109.DescribeDomainsResponse.Domain;
+import com.aliyuncs.alidns.model.v20150109.UpdateDomainRecordRequest;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.github.kevinsawicki.http.HttpRequest;
+
+/**
+ * dns-update-aliyun
+ * 
+ * @author xpoll
+ * @date 2018年12月18日
+ * @url https://xpoll.blmdz.cn
+ * @accessKey
+ * <pre>
+ * accessKey 获取
+ * 
+ * 控制台 -> 头像下accessKey -> 子用户
+ * 或下面链接
+ * https://ram.console.aliyun.com/users
+ * 
+ * 新建 编程访问打勾, 建好就有了, 需要授权
+ * 授权, 搜索dns都打兑上, 确定over
+ * 
+ * 用户 -> 选择新建的用户 下面有用户AccessKey可禁用或新增
+ * </pre>
+ */
+public class DemoListDomains {
+    
+    private static IAcsClient client = null; // 客户端
+
+    private static String dotype = null; // 必须 -- 定义方法 支持 query update
+    private static String accessKeyId = null; // 必须 -- 阿里云key
+    private static String accessKeySecret = null; // 必须 -- 阿里云Secret
+    private static String IP_URL = null; // 修改必须 -- 能获取外网的地址需要返回json格式
+    private static String IP_CODE = null; // 修改必须 -- 返回json格式ip的key
+    private static String recordId = null; // 修改必须 -- 修改value值的解析id
+
+    /**
+     * <pre>
+     * mvn clean install
+     * java -jar target/dns-update-aliyun-0.0.1-SNAPSHOT.jar query accessKeyId accessKeySecret
+     * java -jar target/dns-update-aliyun-0.0.1-SNAPSHOT.jar update accessKeyId accessKeySecret IP_URL IP_CODE recordId
+     * IP_URL 返回参数需要是json 并且包含ip地址, 不支持其他方式
+     * 
+     * 
+     * nginx.conf
+     * 
+     * location = /ip {
+     *   default_type application/json;
+     *   return 200 '{"ip":"$remote_addr"}';
+     * }
+     * 
+     * 如: https://blmdz.cn/ip 返回结构为{"ip": "127.0.0.1"}
+     * 
+     * </pre>
+     */
+    public static void main(String[] args) throws InterruptedException {
+        if (args == null || args.length < 1) {
+            System.out.println("参数不正确哦");
+            return ;
+        }
+        
+        dotype = args[0];
+        
+        if ("query".equals(dotype)) {
+            if (args.length < 3) {
+                System.out.println("参数不正确哦");
+                return ;
+            }
+        } else if ("update".equals(dotype)) {
+            if (args.length < 6) {
+                System.out.println("参数不正确哦");
+                return ;
+            }
+        } else {
+            System.out.println("没有实现该方法");
+            return ;
+        }
+        
+        accessKeyId = args[1];
+        accessKeySecret = args[2];
+        
+        client = new DefaultAcsClient(DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret));
+        
+        if ("query".equals(dotype)) {
+            show(client);
+        } else if ("update".equals(dotype)) {
+            
+            IP_URL = args[3];
+            IP_CODE = args[4];
+            recordId = args[5];
+            
+            DescribeDomainRecordInfoResponse response = describeDomainRecordInfo(client, recordId);
+            String rr = response.getRR(), type = response.getType(), value = response.getValue();
+            String body = HttpRequest.get(IP_URL).body();
+            String ip = JSONObject.parseObject(body).getString(IP_CODE);
+            System.out.println(String.format("时间: %s, 原IP: %s, 现IP: %s, %s", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()), value, ip, ip.equals(value) ? "不需要更新" : "需要更新"));
+            if (!ip.equals(value)) {
+                System.out.println(updateDomainRecord(client, recordId, rr, type, ip) ? "更新成功" : "更新失败");
+            }
+        }
+    }
+
+    /**
+     * 获取domain和详细列表
+     */
+    public static void show(IAcsClient client) {
+
+        List<Domain> domains = describeDomains(client);
+        if (domains != null && !domains.isEmpty()) {
+            domains.forEach(domain -> {
+                System.out.println(String.format("%s: %s", domain.getDomainId(), domain.getDomainName()));
+                List<Record> records = describeDomainRecords(client, domain.getDomainName());
+                if (records != null && !records.isEmpty()) {
+                    records.forEach(record -> {
+                        System.out.println(String.format(
+                                "%s %s %s %s", record.getType(), record.getRecordId(), "@".equals(record.getRR())
+                                        ? record.getRR() : record.getRR() + "." + domain.getDomainName(),
+                                record.getValue()));
+                    });
+                }
+            });
+        }
+    }
+
+    /**
+     * 获取解析记录信息
+     * 
+     * https://help.aliyun.com/document_detail/29777.html
+     */
+    public static DescribeDomainRecordInfoResponse describeDomainRecordInfo(IAcsClient client, String recordId) {
+        DescribeDomainRecordInfoRequest request = new DescribeDomainRecordInfoRequest();
+        request.setActionName("DescribeDomainRecordInfo");
+        request.setRecordId(recordId);
+        try {
+            return client.getAcsResponse(request);
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 修改解析记录
+     * 
+     * https://help.aliyun.com/document_detail/29774.html
+     */
+    public static boolean updateDomainRecord(IAcsClient client, String recordId, String rr, String type, String value) {
+        UpdateDomainRecordRequest request = new UpdateDomainRecordRequest();
+        request.setActionName("UpdateDomainRecord");
+        request.setRecordId(recordId);
+        request.setRR(rr);
+        request.setType(type);
+        request.setValue(value);
+        try {
+            client.getAcsResponse(request);
+            return true;
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 获取解析记录列表
+     * 
+     * https://help.aliyun.com/document_detail/29776.html
+     */
+    public static List<Record> describeDomainRecords(IAcsClient client, String domainName) {
+        DescribeDomainRecordsRequest request = new DescribeDomainRecordsRequest();
+        request.setActionName("DescribeDomainRecords");
+        request.setDomainName(domainName);
+        request.setPageSize(200L);
+        try {
+            return client.getAcsResponse(request).getDomainRecords();
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获取域名列表
+     * 
+     * https://help.aliyun.com/document_detail/29751.html
+     */
+    public static List<Domain> describeDomains(IAcsClient client) {
+        DescribeDomainsRequest request = new DescribeDomainsRequest();
+        try {
+            return client.getAcsResponse(request).getDomains();
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
